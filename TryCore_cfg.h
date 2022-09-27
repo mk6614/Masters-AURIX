e3fda66224ffd334f7b823013ced88bc5246a36e
Mojca Kolšek <mk6614@student.uni-lj.si>
2022-09-27 13:00:13 +0100

    global definitions

---
+++ TryCore_cfg.h
@@ -0,0 +1,39 @@
+/*
+ * TryCore_cfg.h
+ *
+ *  Created on: 20 Jul 2021
+ *      Author: mojca
+ */
+
+#ifndef TRYCORE_CFG_H_
+#define TRYCORE_CFG_H_
+
+//test case size
+#define TryC_WORDS 4096
+
+//#define PRINT_MICROSECONDS
+//#define PRINT_M2
+//#define PRINT_S
+#define PRINT_VAR
+#define SEPARATOR   ","
+#define NEW_LINE    "\n\r"
+
+//if MCCNT1, 2 and 3 count values
+#define C123
+
+//define the addres of starting
+#define FLASH_MEM_ADDRES 0xAF000000
+
+//#define LMU_MEMORY
+//#define DLMU_MEMORY
+
+#define TryC_1GEN
+
+
+
+
+
+
+
+
+#endif /* TRYCORE_CFG_H_ */
